e21a2b404cdc7ebda0d11874066fc70098181d78
Ramses Tovar <ramses.tovar@gmail.com>
2016-04-24 22:52:04 -0430

    Rails 4 with AngularJS project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+FakeLunchHub::Application.config.secret_token = '80a3c3c3c419d63715e977c55eb01175e4b9c6aa651435efbfab84f5fd4fe7dd15e8f114518b76d1ace7fc88b985aaf3dca3b404b8b56f5905644169cdd637f8'
